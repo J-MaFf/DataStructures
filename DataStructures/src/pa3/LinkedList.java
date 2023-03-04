a03d9ae447ec6dbb7d6200432e6a1cd06264cd3e
Joey M <7maffiolajoey@gmail.com>
2023-03-04 15:21:51 -0600

    Project 3 complete!
    mergeSort() method complete and in working order. Project 3 complete

--- DataStructures/src/pa3/LinkedList.java
+++ DataStructures/src/pa3/LinkedList.java
@@ -16,7 +16,55 @@ public class LinkedList {
 		mergesort(0, size() - 1, head, tail);
 	}
 
+	/**
+	 * "mergesort" method:
+	 * 
+	 * @param left
+	 * @param right
+	 * @param leftNode
+	 * @param rightNode
+	 */
 	private void mergesort(int left, int right, ListNode leftNode, ListNode rightNode) { // complete this function
+		if (left >= right) { // if LL is of length 1
+			return;
+		}
+		int mid = (left + right) / 2;
+		ListNode midNode = leftNode;
+		// Move the place-holder using a loop from i = left to i < mid such that midNode points to the node at index mid
+		for (int i = left; i < mid; i++) {
+			midNode = midNode.next;
+		} // end for
+			// Now that midNode is the midNode, we can recursivley call mergeSort
+		mergesort(left, mid, leftNode, midNode);
+		mergesort(mid + 1, right, midNode.next, rightNode);
+		// Initialize tracker vars
+		int k = left;
+		ListNode tmp1 = leftNode, tmp2 = midNode.next;
+		while (tmp1 != midNode.next && tmp2 != rightNode.next) { // While there are nodes left in either sublist
+			if (tmp1.value < tmp2.value) {
+				mergedArray[k++] = tmp1.value;
+				tmp1 = tmp1.next;
+			} else { // tmp2.value is >= tmp1.value
+				mergedArray[k++] = tmp2.value;
+				tmp2 = tmp2.next;
+			}
+		}
+		// At this point, one of the sublists is empty; now we copy the remaining list to our merged aray
+		while (tmp1 != midNode.next) { // while there are still nodes in the first sublist
+			mergedArray[k++] = tmp1.value;
+			tmp1 = tmp1.next;
+		}
+		while (tmp2 != rightNode.next) {
+			mergedArray[k++] = tmp2.value;
+			tmp2 = tmp2.next;
+		}
+		k = left; // reset k
+		ListNode tmp = leftNode; // tmp represents current ListNode
+		// Use while loop to fill LL (i think)
+		while (tmp != rightNode.next) {
+			tmp.value = mergedArray[k++];
+			tmp = tmp.next;
+		}
 	}
 
 	public ListNode insertAtFront(int value) {
