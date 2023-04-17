f3932af77c2db08048e2b9b6088115e076c3ed8a
Joey M <7maffiolajoey@gmail.com>
2023-04-17 15:31:35 -0500

    Updated SetsAndMaps.java
    sortedAlphabet and bstSort are complete & working

--- DataStructures/src/pa5/SetsAndMaps.java
+++ DataStructures/src/pa5/SetsAndMaps.java
@@ -9,9 +9,57 @@ import java.util.TreeSet;
 
 public class SetsAndMaps {
 
-    public static ArrayList<Character> sortedAlphabet(char[] arr, int len) {} // complete this method
+    /**
+     * returns the alphabet of a given array of characters in sorted order, i.e.,
+     * this method returns the distinct characters in the array in sorted order.
+     *
+     * The time complexity of this method is O(n log n)
+     *
+     * @param arr the array of characters
+     * @param len the length of the array
+     * @return the alphabet of the array in sorted order
+     */
+    public static ArrayList<Character> sortedAlphabet(char[] arr, int len) {
+        TreeSet<Character> charSet = new TreeSet<>();
+        ArrayList<Character> charList = new ArrayList<>();
+        // take all the characters from the array and insert them into the ordered set
+        for (int i = 0; i < len; i++) { // O(n)
+            charSet.add(arr[i]); // O(log n)
+        }
+        // Sets only keep unique characters, so we can just iterate through the set and add them to the list
+        Iterator<Character> it = charSet.iterator();
+        while (it.hasNext()) {
+            charList.add(it.next());
+        }
+        return charList;
+    }
 
-    public static void bstSort(char[] arr, int len) {} // complete this method
+    /**
+     * Sorts an array of characters in ascending order using a tree map. each character
+     * must appear as many time as present in the original array
+     */
+    public static void bstSort(char[] arr, int len) {
+        // Create an ordered map to store the characters and their frequencies
+        TreeMap<Character, Integer> charMap = new TreeMap<>();
+        for (int i = 0; i < len; i++) {
+            if (!charMap.containsKey(arr[i])) { // Key not found
+                charMap.put(arr[i], 1);
+            } else { // Key found
+                charMap.put(arr[i], charMap.get(arr[i]) + 1);
+            }
+        }
+        // Now we can iterate through the map and add the characters to the array
+        int index = 0;
+        Iterator it = charMap.keySet().iterator();
+        while (it.hasNext()) {
+            char key = (char) it.next();
+            int freq = charMap.get(key);
+            for (int i = 0; i < freq; i++) {
+                arr[index] = key;
+                index++;
+            }
+        }
+    }
 
     public static boolean zeroSumSubArray(int[] arr, int len) {} // complete this method
 
--- DataStructures/src/pa5/customTest.java
+++ DataStructures/src/pa5/customTest.java
@@ -1,40 +1,38 @@
 package pa5;
 
+import java.util.Arrays;
+
 public class customTest {
 
     public static void main(String[] args) {
-        BinaryTreeNode node_22 = new BinaryTreeNode(22);
-        BinaryTreeNode node_8 = new BinaryTreeNode(8);
-        BinaryTreeNode node_16 = new BinaryTreeNode(16);
-        BinaryTreeNode node_14 = new BinaryTreeNode(14);
-        BinaryTreeNode node_20 = new BinaryTreeNode(20);
-        BinaryTreeNode node_24 = new BinaryTreeNode(24);
-        BinaryTreeNode node_8_2 = new BinaryTreeNode(8);
-        BinaryTreeNode node_0 = new BinaryTreeNode(0);
-        BinaryTreeNode node_6 = new BinaryTreeNode(6);
-        BinaryTreeNode node_11 = new BinaryTreeNode(11);
-        BinaryTreeNode node_20_2 = new BinaryTreeNode(20);
-        BinaryTreeNode node_5 = new BinaryTreeNode(5);
-        BinaryTreeNode node_70 = new BinaryTreeNode(70);
+        testOrdered();
+    }
 
-        node_22.left = node_8;
-        node_22.right = node_16;
-        node_8.left = node_14;
-        node_8.right = node_20;
-        node_16.left = node_24;
-        node_16.right = node_8_2;
-        node_14.left = node_0;
-        node_14.right = node_6;
-        node_20.right = node_11;
-        node_8_2.right = node_20_2;
-        node_6.left = node_5;
-        node_6.right = node_70;
+    public static void testOrdered() {
+        System.out.println("*** Using Ordered Set & Map ***\n");
+        char[] str = {
+            'a',
+            'b',
+            'r',
+            'a',
+            'c',
+            'a',
+            'd',
+            'a',
+            'b',
+            'r',
+            'a',
+            '$',
+        };
+        System.out.println(
+            "Alphabet of " +
+            Arrays.toString(str) +
+            " is " +
+            SetsAndMaps.sortedAlphabet(str, str.length)
+        );
 
-        TreeTraversals.preOrder(node_22);
-        System.out.println("");
-        TreeTraversals.inOrder(node_22);
-        System.out.println("");
-        TreeTraversals.postOrder(node_22);
-        System.out.println("");
+        System.out.print("Sorted order of " + Arrays.toString(str) + " is ");
+        SetsAndMaps.bstSort(str, str.length);
+        System.out.println(Arrays.toString(str));
     }
 }
