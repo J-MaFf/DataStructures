826208f496f7c6226f824014743d9674c556a785
Joey M <7maffiolajoey@gmail.com>
2023-04-17 15:56:01 -0500

    SetsAndMaps.java complete and working

--- DataStructures/src/pa5/SetsAndMaps.java
+++ DataStructures/src/pa5/SetsAndMaps.java
@@ -61,7 +61,67 @@ public class SetsAndMaps {
         }
     }
 
-    public static boolean zeroSumSubArray(int[] arr, int len) {} // complete this method
+    /**
+     * checks if an array contains a subarray whose sum is zero
+     *
+     * The time complexity of this method is O(n)
+     * @param arr the array of integers
+     * @param len the length of the array
+     * @return true if the array contains a subarray whose sum is zero, false otherwise
+     */
+    public static boolean zeroSumSubArray(int[] arr, int len) {
+        // Create an integer UNORDERED set.
+        HashSet<Integer> unorderedSet = new HashSet<>();
+        int prefixSum = 0;
+        for (int i = 0; i < len; i++) {
+            prefixSum += arr[i];
+            // If the prefix sum is 0, then we found a subarray with sum 0
+            if (prefixSum == 0) {
+                return true;
+            }
+            // If the prefix sum is already in the set, then we found a subarray with sum 0
+            if (unorderedSet.contains(prefixSum)) {
+                return true;
+            }
+            // Add the prefix sum to the set because we haven't found a subarray with sum 0 yet
+            unorderedSet.add(prefixSum);
+        }
+        // If we reach this point, then no subarray with sum 0 was found
+        return false;
+    }
 
-    public static ArrayList<Integer> kHeavyHitters(int[] arr, int len, int k) {} // complete this method\
+    /**
+     * returns the k heavy hitters in an array of integers. A heavy hitter is an
+     * integer that appears more than n/k times in the array.
+     *
+     * @param arr the array of integers
+     * @param len the length of the array
+     * @param k the value of k
+     * @return  an array list containing the k heavy hitters in the array
+     */
+    public static ArrayList<Integer> kHeavyHitters(int[] arr, int len, int k) {
+        // Create an integer UNORDERED map
+        HashMap<Integer, Integer> unorderedMap = new HashMap<>();
+        // Compute the frequencies of each number in the array and store them in the map
+        for (int i = 0; i < len; i++) {
+            if (!unorderedMap.containsKey(arr[i])) { // Key not found
+                unorderedMap.put(arr[i], 1);
+            } else { // Key found
+                unorderedMap.put(arr[i], unorderedMap.get(arr[i]) + 1);
+            }
+        }
+        ArrayList<Integer> list = new ArrayList();
+        // Iterate through the map and add the keys to the list if their frequency is greater than len / k
+        Iterator it = unorderedMap.keySet().iterator();
+        while (it.hasNext()) {
+            int key = (int) it.next();
+            int freq = unorderedMap.get(key);
+            if (freq > len / k) {
+                list.add(key);
+                // Set the frequency of the key to 0 so that we don't add it again
+                unorderedMap.put(key, 0);
+            }
+        }
+        return list;
+    }
 }
--- DataStructures/src/pa5/customTest.java
+++ DataStructures/src/pa5/customTest.java
@@ -6,6 +6,11 @@ public class customTest {
 
     public static void main(String[] args) {
         testOrdered();
+        System.out.println();
+        testZeroSum();
+        System.out.println();
+        testHeavyHitters();
+        System.out.println();
     }
 
     public static void testOrdered() {
@@ -34,5 +39,44 @@ public class customTest {
         System.out.print("Sorted order of " + Arrays.toString(str) + " is ");
         SetsAndMaps.bstSort(str, str.length);
         System.out.println(Arrays.toString(str));
+    }
+
+    private static void testZeroSumHelper(int[] arr, int len) {
+        boolean isZero = SetsAndMaps.zeroSumSubArray(arr, len);
+        System.out.printf("\n%s", Arrays.toString(arr));
+        if (!isZero) System.out.printf(
+            " has no subarray whose sum is zero."
+        ); else System.out.printf(" has a subarray whose sum is zero.");
+    }
+
+    private static void testZeroSum() {
+        System.out.println("*** Using Unordered Set & Map ***");
+        int[] arr1 = { 12, -26, 1, 8, 9, -6, 4, -12, -3, 12 };
+        testZeroSumHelper(arr1, arr1.length);
+        int[] arr2 = { 1, 7, 19, -14, 1, -14, 12 };
+        testZeroSumHelper(arr2, arr2.length);
+        int[] arr3 = { 1, 7, 19, -14, 1, -14, 8, 9, -6, 4, -12, -3, 12 };
+        testZeroSumHelper(arr3, arr3.length);
+        int[] arr4 = { -6, 4, -12, -3, 12 };
+        testZeroSumHelper(arr4, arr4.length);
+        int[] arr5 = { -6, 4, -12, 0, -3, 12 };
+        testZeroSumHelper(arr5, arr5.length);
+        int[] arr6 = { 0, -6, 4, -12, -3, 12 };
+        testZeroSumHelper(arr6, arr6.length);
+        int[] arr7 = { 1, 8, 9, -6, 4, -12, -3 };
+        testZeroSumHelper(arr7, arr7.length);
+        int[] arr8 = { 1, 8, 9, 0 };
+        testZeroSumHelper(arr8, arr8.length);
+    }
+
+    private static void testHeavyHitters() {
+        System.out.println();
+        int[] arr = { 5, 3, 4, 5, 1, 3, 5, 3, 9, 4, 9, 2, 3, 8, 3, 0, 9, 3, 9 };
+        System.out.println("Array is: " + Arrays.toString(arr));
+        for (int i = 2; i <= 9; i++) System.out.printf(
+            "%d-heavy hitters are: %s\n",
+            i,
+            SetsAndMaps.kHeavyHitters(arr, arr.length, i)
+        );
     }
 }
