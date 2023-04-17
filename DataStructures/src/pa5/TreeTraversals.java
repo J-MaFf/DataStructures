223ceb07e8d8651e2469d93dde3a827062785f00
Joey M <7maffiolajoey@gmail.com>
2023-04-17 13:23:23 -0500

    Starting PA5

---
+++ DataStructures/src/pa5/TreeTraversals.java
@@ -0,0 +1,10 @@
+package pa5;
+
+public class TreeTraversals {
+
+    public static void preOrder(BinaryTreeNode node) {} // complete this method
+
+    public static void inOrder(BinaryTreeNode node) {} // complete this method
+
+    public static void postOrder(BinaryTreeNode node) {} // complete this method
+}
