34ea520e12e45fb4abb75a9ede65913d7bf95ac6
Joey M <7maffiolajoey@gmail.com>
2023-04-18 11:01:22 -0500

    readLanguage method complete
    The method readLanguage takes a file path as a parameter and reads the contents of the file to extract information about a language. Here's how the method works:
    
        The method first creates a new Scanner object that reads from a file specified by the filePath parameter.
        The method then reads the first two integers from the file using the nextInt() method of the Scanner object. These two integers represent the number of vertices and the number of words in the language, respectively.
        The method initializes a new String array called words with a length of numWords, which is the number of words read from the file.
        The method then loops numWords times, and in each iteration, reads a String value from the file using the next() method of the Scanner object and stores it in the words array.
        Finally, the method closes the Scanner object to release any system resources associated with it.
    
    This method can be used as part of a larger program that reads and processes language data from a file. After the readLanguage method is called, the numVertices, numWords, and words variables will contain the relevant information about the language that can be used for further analysis or processing. Typically, after reading the language data, the program would create a graph or other data structure to represent the language and perform operations on it, such as searching for patterns or generating new words.

--- DataStructures/src/pa5/MutantLanguage.java
+++ DataStructures/src/pa5/MutantLanguage.java
@@ -18,7 +18,21 @@ public class MutantLanguage extends Graph {
         makeGraph();
     }
 
-    private void readLanguage(String filePath) throws FileNotFoundException {} // complete this method
+    /**
+     * Reads the language from the file and stores it in the words array.
+     * @param filePath the path to the file
+     * @throws FileNotFoundException if the file is not found
+     */
+    private void readLanguage(String filePath) throws FileNotFoundException {
+        Scanner fileReader = new Scanner(new FileInputStream(filePath));
+        numVertices = fileReader.nextInt();
+        numWords = fileReader.nextInt();
+        words = new String[numWords];
+        for (int i = 0; i < numWords; i++) {
+            words[i] = fileReader.next();
+        }
+        fileReader.close();
+    }
 
     private void makeGraph() {} // complete this method
 
